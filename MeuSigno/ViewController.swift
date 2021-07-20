16a6e874de6226a16d9a82d4397ea9997454a966
athena <athena@ciandt.com>
2021-07-20 14:49:24 -0300

    iniciando projeto

---
+++ MeuSigno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeuSigno
+//
+//  Created by Athena Fernandes Sarantôpoulos on 20/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
